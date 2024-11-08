07ab4bcf31da36a3e6be5e79460feefd173a8ce4
dada1919 <986445030@qq.com>
2024-11-08 10:25:30 +0800

    比较视图

--- src/main.js
+++ src/main.js
@@ -4,9 +4,13 @@ import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import { createPinia } from 'pinia'
 // import { createPinia } from 'pinia'
+import * as Icons from '@element-plus/icons-vue'
 
 
 const app = createApp(App);
+for (let i in Icons) {
+    app.component(i, Icons[i])
+  }
 app.use(createPinia());
 app.use(ElementPlus);
 app.mount('#app');
